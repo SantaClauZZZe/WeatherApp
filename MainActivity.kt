4120b1cf240ef877b488d8ae0360690412cbf196
SantaClauZZZe <119117293+SantaClauZZZe@users.noreply.github.com>
2022-12-22 12:45:40 +0800

    update

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,6 +15,7 @@ import java.io.InputStream
 import java.net.URL
 import java.util.*
 import androidx.databinding.DataBindingUtil
+import kotlinx.coroutines.CoroutineScope
 import org.json.JSONObject
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
@@ -50,40 +52,48 @@ class MainActivity : AppCompatActivity() {
 
         if (!is_update)
         {
-            var city = binding.cityName.text.toString()
+            val city = binding.cityName.text.toString()
             this._city = if (city == "") { "Irkutsk" } else { city }
         }
 
         val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=${this._city}&appid=${API_KEY}&units=metric";
         var data = ""
 
-        try {
-            val stream = URL(weatherURL).getContent() as InputStream
+        val weather = Weather()
+        weather.city = this._city
+        lateinit var stream : InputStream
 
-            data = Scanner(stream).nextLine() ?: ""
+        try {
+            stream = URL(weatherURL).getContent() as InputStream
         } catch (e : IOException){
-            print("Error Internet connection!")
+            weather.city = "Error Internet connection!"
         }
 
-        var weather = Weather()
+        try {
+            data = Scanner(stream).nextLine() ?: ""
 
-        weather.city = this._city
+            val jsonObj = JSONObject(data)
 
-        val jsonObj = JSONObject(data)
-        weather.temp = jsonObj.getJSONObject("main").getString("temp").let{ "$it°" } ?: "Error"
-        weather.temp_like_as = jsonObj.getJSONObject("main").getString("feels_like").let{ "$it°" } ?: "Error"
-        weather.wind_speed = jsonObj.getJSONObject("wind").getString("speed").let{ "$it м/с" } ?: "Error"
-        weather.wind_degrees = jsonObj.getJSONObject("wind").getString("deg").let{ "$it°" } ?: "Error"
-        weather.humidity = jsonObj.getJSONObject("main").getString("humidity").let{ "$it%" } ?: "Error"
-        val temp = (jsonObj.getJSONObject("sys").getString("sunset") ?: "0").toLong() -
-                (jsonObj.getJSONObject("sys").getString("sunrise") ?: "0").toLong()
-        weather.day_duration = "${(Duration.convert(temp.toDouble(), DurationUnit.SECONDS, DurationUnit.HOURS)).toInt().toString()} ч. : " +
-                               "${(Duration.convert(temp.toDouble() % 3600, DurationUnit.SECONDS, DurationUnit.MINUTES)).toInt().toString()} м."
+            weather.temp = jsonObj.getJSONObject("main").getString("temp").let{ "$it°" }
+            weather.temp_like_as = jsonObj.getJSONObject("main").getString("feels_like").let{ "$it°" }
+            weather.wind_speed = jsonObj.getJSONObject("wind").getString("speed").let{ "$it м/с" }
+            weather.wind_degrees = jsonObj.getJSONObject("wind").getString("deg").let{ "$it°" }
+            weather.humidity = jsonObj.getJSONObject("main").getString("humidity").let{ "$it%" }
+            val temp = (jsonObj.getJSONObject("sys").getString("sunset") ?: "0").toLong() -
+                    (jsonObj.getJSONObject("sys").getString("sunrise") ?: "0").toLong()
+            weather.day_duration = "${(Duration.convert(temp.toDouble(), DurationUnit.SECONDS, DurationUnit.HOURS)).toInt().toString()} ч. : " +
+                    "${(Duration.convert(temp.toDouble() % 3600, DurationUnit.SECONDS, DurationUnit.MINUTES)).toInt().toString()} м."
+        }
+        catch (e : Exception){
+            if (weather.city == "") {
+                weather.city = "Ошибка запроса! (проверьте название города)"
+            }
+        }
 
         binding.weather = weather
 
-        Log.d("mytag", data)
-        Log.d("mytag", temp.toString())
+//        Log.d("mytag", data)
+//        Log.d("mytag", temp.toString())
     }
 
     public fun onClick(is_update : Boolean = false) {
